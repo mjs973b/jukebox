mjs973b/jukebox

commit 8304afa9042ee64459c844696e67b470d1c17ad7

playlist: replace Playlist::readOnly() by more fine-grained permissions
readOnly() is very, very restrictive. This property affects what THE USER
is allowed to do from the menus and keyboard. It:
- prohibits any changes to the playlist content
- prohibits deletion of the playlist itself
- prohibits renaming the playlist
- prohibits saving the playlist to disk

It's so restrictive that it's almost useless. Replace it by three new
finer-grain methods, which we'll take full advantage of in a future patch:

Playlist::canDelete()         # delete the playlist and the .m3u file itself
Playlist::canModifyContent()  # add,delete,rename within the list
Playlist::canRename()         # edit the label on playlist icon

This patch also replaces all calls to readOnly() by the proper new
method. The visible application behavior should be changed.

playlist.cpp
@@ -2206,7 +2206,7 @@ void Playlist::slotShowRMBMenu(Q3ListViewItem *item, const QPoint &point, int co
             i18n("Add to Play Queue"), this, SLOT(slotAddToUpcoming()));
         m_rmbMenu->addSeparator();
 
-        if(!readOnly()) {
+        if(!this->canModifyContent()) {
             m_rmbMenu->addAction( action("edit_cut") );
             m_rmbMenu->addAction( action("edit_copy") );
             m_rmbMenu->addAction( action("edit_paste") );

historyplaylist.h
@@ -48,7 +48,9 @@ public:
                                             bool emitChanged = true);
     virtual void createItems(const PlaylistItemList &siblings);
     virtual int columnOffset() const { return 1; }
-    virtual bool readOnly() const { return true; }
+    virtual bool canDelete() const { return false; }
+    virtual bool canModifyContent() const { return false; }
+    virtual bool canRename() const { return false; }
 
     static int delay() { return 5000; }
 

playlist.h
@@ -292,14 +292,31 @@ public:
     virtual int columnOffset() const { return 0; }
 
     /**
-     * Some subclasses of Playlist will be "read only" lists (i.e. the history
-     * playlist).  This is a way for those subclasses to indicate that to the
-     * Playlist internals.
+     * Report whether the content of this playlist is permitted to be 
+     * modified i.e. whether .mp3 files can be added, deleted or renamed 
+     * for this .m3u playlist BY USER ACTION. The default is true, but it 
+     * can be changed by a subclass.
      */
-    virtual bool readOnly() const { return false; }
+    virtual bool canModifyContent() const { return true; }
 
     /**
-     * Returns true if it's possible to reload this playlist.
+     * Report whether the label on this playlist is permiteed to be 
+     * changed BY USER ACTION. The default is true, but it can be changed 
+     * by a subclass.
+     */
+    virtual bool canRename() const { return true; }
+
+    /**
+     * Report whether this playlist is permitted to be deleted (both in 
+     * RAM and on disk) BY USER ACTION. The default is true, but it be 
+     * changed by a subclass.
+     */
+    virtual bool canDelete() const { return true; }
+
+    /**
+     * Returns true if it's permitted to reread this playlist from
+     * disk. This method does not verify the underlying file still
+     * exists.
      */
     virtual bool canReload() const { return !m_fileName.isEmpty(); }
 

playlistbox.cpp
@@ -313,6 +313,10 @@ void PlaylistBox::saveConfig()
     KGlobal::config()->sync();
 }
 
+/**
+ * Remove selected playlists. Prompt the user whether to remove the .m3u 
+ * file from disk too.
+ */
 void PlaylistBox::remove()
 {
     ItemList items = selectedBoxItems();
@@ -370,7 +374,7 @@ void PlaylistBox::remove()
     for(ItemList::ConstIterator it = items.constBegin(); it != items.constEnd(); ++it) {
         if(*it != Item::collectionItem() &&
            (*it)->playlist() &&
-           (!(*it)->playlist()->readOnly()))
+           ((*it)->playlist()->canDelete()))
         {
             removeQueue.append((*it)->playlist());
         }
@@ -451,7 +455,7 @@ void PlaylistBox::savePlaylistsToDisk(bool bDialogOk)
     for(Q3ListViewItem *i = this->firstChild(); i; i = i->nextSibling()) {
         Item *item = static_cast<Item *>(i);
         pl = item->playlist();
-        if(pl && pl != collection && !pl->readOnly() && 
+        if(pl && pl != collection && pl->canModifyContent() && 
            pl->hasFileListChanged()) {
            
             int retval;
@@ -505,10 +509,17 @@ void PlaylistBox::slotRemoveItem(const QString &tag, unsigned column)
         (*it)->removeItem(tag, column);
 }
 
+/**
+ * Handle the "Drop" part of Drag/Drop. 'this' points to the playlist
+ * which is being dropped onto. The QMimeData contains a list of absolute 
+ * filename(s).
+ */
 void PlaylistBox::decode(const QMimeData *s, Item *item)
 {
-    if(!s || (item && item->playlist() && item->playlist()->readOnly()))
+    if(!s || 
+       (item && item->playlist() && !item->playlist()->canModifyContent())) {
         return;
+    }
 
     const KUrl::List urls = KUrl::List::fromMimeData(s);
 
@@ -571,7 +582,7 @@ void PlaylistBox::contentsDragMoveEvent(QDragMoveEvent *e)
 
     if(target) {
 
-        if(target->playlist() && target->playlist()->readOnly())
+        if(target->playlist() && !target->playlist()->canModifyContent())
             return;
 
         // This is a semi-dirty hack to check if the items are coming from within
@@ -707,10 +718,19 @@ void PlaylistBox::slotPlaylistChanged()
 
     bool singlePlaylist = playlists.count() == 1;
 
+    bool bReadOnly = false;
+    if (singlePlaylist) {
+        Playlist *pl = playlists.front();
+        /* for now, we exactly re-create the behavior of Playlist::readOnly().
+         * Only the HistoryPlaylist class will evaluate to true.
+         */
+        bReadOnly = !pl->canDelete() && !pl->canModifyContent() && !pl->canRename(); 
+    }
+
     if(playlists.isEmpty() ||
        (singlePlaylist &&
         (playlists.front() == CollectionList::instance() ||
-         playlists.front()->readOnly())))
+         bReadOnly)))
     {
         action("file_save")->setEnabled(false);
         action("file_save_as")->setEnabled(false);

commit 4b05df6e4d230a8523badd232aadb4c49cb39512

fix: Playlist Widget Popup Menu items are populated incorrectly
Bug introduced in commit 8304afa9 when readOnly() was replaced by
canModifyContent(). The bang should have been removed too.

playlist.cpp
@@ -2236,7 +2236,7 @@ void Playlist::slotShowRMBMenu(Q3ListViewItem *item, const QPoint &point, int co
             i18n("Add to Play Queue"), this, SLOT(slotAddToUpcoming()));
         m_rmbMenu->addSeparator();
 
-        if(!this->canModifyContent()) {
+        if(this->canModifyContent()) {
             m_rmbMenu->addAction( action("edit_cut") );
             m_rmbMenu->addAction( action("edit_copy") );
             m_rmbMenu->addAction( action("edit_paste") );