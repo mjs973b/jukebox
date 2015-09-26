cc73fbce6a22589e0a162fe48bf19cda06c7803a
Mike Scheutzow <mjs973@gmail.com>
2015-09-26 09:03:36 -0400

    fix: CollectionList was incorrectly allowing duplicate tracks
    Yesterday I unintentionally commited an experiment where the Playlist
    constructor defaulted to 'allow duplicate track' entries. In the big picture,
    this change seems like a good idea, as the only Playlist that really seems to
    need the restriction is CollectionList.
    
    This patch restores the 'no dups' behavior for CollectionList.
    
    Here is the commit where I broke it:
    
    commit 4c36fdfde6d03e1307b369b4ffe1c6f74dbb858a
    Author: Mike Scheutzow <mjs973@gmail.com>
    Date:   Fri Sep 25 20:19:46 2015 -0400
    
        playlist: change default sort to 'insert order'

--- collectionlist.cpp
+++ collectionlist.cpp
@@ -345,7 +345,8 @@ CollectionList::CollectionList(PlaylistCollection *collection) :
             this, SLOT(slotPopulateBackMenu()));
     connect(action<KToolBarPopupAction>("back")->menu(), SIGNAL(triggered(QAction*)),
             this, SLOT(slotPlayFromBackMenu(QAction*)));
-    setSorting(-1); // Temporarily disable sorting to add items faster.
+
+    setAllowDuplicates(false);
 
     m_columnTags[PlaylistItem::ArtistColumn] = new TagCountDict;
     m_columnTags[PlaylistItem::AlbumColumn] = new TagCountDict;
