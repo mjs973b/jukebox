mjs973b/jukebox

commit e7ca98de76b19e70046c62fe5cfdbb7aecb69077

refactor: comment out useless URL Drop code
This code makes no sense. It is invoked when one or more URLs are dropped
onto the track table. But for that to happen, the track table must
already be visible. So the playlist icon is already selected. So this
code accomplishes nothing.

Commented rather than deleted in case we find a situation which
actually needs it.

playlistbox.cpp
@@ -325,8 +325,8 @@ void PlaylistBox::slotSetHistoryPlaylistEnabled(bool enable)
 
 void PlaylistBox::setupPlaylist3(Playlist *playlist, const QString &iconName, Item *parentItem)
 {
-    connect(playlist, SIGNAL(signalPlaylistItemsDropped(Playlist*)),
-            SLOT(slotPlaylistItemsDropped(Playlist*)));
+    //connect(playlist, SIGNAL(signalPlaylistItemsDropped(Playlist*)),
+    //        SLOT(slotPlaylistItemsDropped(Playlist*)));
 
     PlaylistCollection::instance()->setupPlaylist2(playlist, iconName);
 
@@ -1014,9 +1014,15 @@ void PlaylistBox::slotShowContextMenu(Q3ListViewItem *, const QPoint &point, int
     m_contextMenu->popup(point);
 }
 
+/* This method is called when URLs are dropped on the track table (note: not
+ * on this widget).
+ *
+ * The catch is, the track table must be visible for this to happen. So the
+ * playlist icon is already selected. So this method is always unnecessary.
+ */
 void PlaylistBox::slotPlaylistItemsDropped(Playlist *p)
 {
-    this->raise2(p);
+    //this->raise2(p);
 }
 
 void PlaylistBox::slotSetViewMode(int index)

commit b37377e541bbd46e47d6858ddeb9cdd62a83c5cf

refactor: quiet 'unused parameter' warning from gcc
Warning caused by commit e7ca98de
"refactor: comment out useless URL Drop code"

playlistbox.cpp
@@ -1027,6 +1027,7 @@ void PlaylistBox::slotShowContextMenu(Q3ListViewItem *, const QPoint &point, int
  */
 void PlaylistBox::slotPlaylistItemsDropped(Playlist *p)
 {
+    Q_UNUSED(p);
     //this->raise2(p);
 }
 